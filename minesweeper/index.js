1c05cc29b2accc7ca1f551cd381095352690c59a
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2023-02-27 21:33:22 +0300

    feat: add timer function

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,5 +1,17 @@
 const field = document.querySelector(".field");
-
+const numbersPositionArray = [
+  "-126px top",
+  "0px top",
+  "-14px top",
+  "-28px top",
+  "-42px top",
+  "-56px top",
+  "-70px top",
+  "-84px top",
+  "-98px top",
+  "-112px top",
+];
+let timer = 989;
 createField(16, 40);
 
 function createField(width, mines) {
@@ -18,31 +30,66 @@ field.addEventListener("mousedown", (e) => {
   document
     .querySelector(".heading__smile")
     .classList.add("heading__smile_scary");
-    e.target.classList.add('field__button_empty')
+  e.target.classList.add("field__button_empty");
 });
 
 field.addEventListener("mouseup", (e) => {
   document
     .querySelector(".heading__smile")
     .classList.remove("heading__smile_scary");
-    e.target.classList.remove('field__button_empty')
+  e.target.classList.remove("field__button_empty");
 });
 
-field.addEventListener('click', (e) => {
-const cells = Array.from(document.querySelectorAll('.field__button'))
-const currentTargetIndex = cells.indexOf(e.target)
+field.addEventListener("click", (e) => {
+  const cells = Array.from(document.querySelectorAll(".field__button"));
+  const currentTargetIndex = cells.indexOf(e.target);
+  startTimer();
+});
 
-})
+field.addEventListener("contextmenu", (e) => {
+  console.log("cont");
+  if (e.target.classList.contains("field__button_flag")) {
+    e.target.classList = "field__button field__button_question";
+    e.target.disabled = true;
+  } else if (e.target.classList.contains("field__button_question")) {
+    e.target.classList = "field__button";
+    e.target.disabled = false;
+  } else {
+    e.target.classList = "field__button field__button_flag";
+    e.target.disabled = true;
+  }
+});
 
-field.addEventListener('contextmenu', (e) => {
-    console.log('cont')
-    if (e.target.classList.contains('field__button_flag')){
-        e.target.classList = 'field__button field__button_question'
+function startGame() {}
+
+function startTimer() {
+  const firstInputNumber = document.querySelector(".heading__first-time");
+  const secondInputNumber = document.querySelector(".heading__second-time");
+  const thirdInputNumber = document.querySelector(".heading__third-time");
+  const timerInterval = setInterval(() => {
+    timer++;
+    if (timer < 10) {
+      thirdInputNumber.style.backgroundPosition = numbersPositionArray[timer];
+    } else if (timer >= 10 && timer < 100) {
+      const secondNumber = Math.floor(timer / 10);
+      const thirdNumber = timer % 10;
+      secondInputNumber.style.backgroundPosition =
+        numbersPositionArray[secondNumber];
+      thirdInputNumber.style.backgroundPosition =
+        numbersPositionArray[thirdNumber];
+    } else if (timer >= 100 && timer < 1000) {
+      const firstNumber = Math.floor(timer / 100);
+      const secondNumber = Math.floor(timer / 10) % 10;
+      const thirdNumber = (timer % 100) % 10;
+      firstInputNumber.style.backgroundPosition =
+        numbersPositionArray[firstNumber];
+      secondInputNumber.style.backgroundPosition =
+        numbersPositionArray[secondNumber];
+      thirdInputNumber.style.backgroundPosition =
+        numbersPositionArray[thirdNumber];
+      if (timer >= 1000) {
+        clearInterval(timerInterval);
+      }
     }
-    else if (e.target.classList.contains('field__button_question')){
-        e.target.classList = 'field__button'
-    }
-else{
-    e.target.classList = 'field__button field__button_flag'
+  }, 1000);
 }
-})
