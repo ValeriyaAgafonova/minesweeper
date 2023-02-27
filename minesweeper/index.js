627796140ee602e4f1bbe22ed8cc9e7a453eaf8d
ValeriyaAgafonova <sineivolnousvet@yandex.com>
2023-02-27 18:35:19 +0300

    feat: add function to add flag or question

--- minesweeper/index.js
+++ minesweeper/index.js
@@ -14,14 +14,35 @@ function createField(width, mines) {
   console.log(bombs);
 }
 
-field.addEventListener("mousedown", () => {
+field.addEventListener("mousedown", (e) => {
   document
     .querySelector(".heading__smile")
     .classList.add("heading__smile_scary");
+    e.target.classList.add('field__button_empty')
 });
 
-field.addEventListener("mouseup", () => {
+field.addEventListener("mouseup", (e) => {
   document
     .querySelector(".heading__smile")
     .classList.remove("heading__smile_scary");
+    e.target.classList.remove('field__button_empty')
 });
+
+field.addEventListener('click', (e) => {
+const cells = Array.from(document.querySelectorAll('.field__button'))
+const currentTargetIndex = cells.indexOf(e.target)
+
+})
+
+field.addEventListener('contextmenu', (e) => {
+    console.log('cont')
+    if (e.target.classList.contains('field__button_flag')){
+        e.target.classList = 'field__button field__button_question'
+    }
+    else if (e.target.classList.contains('field__button_question')){
+        e.target.classList = 'field__button'
+    }
+else{
+    e.target.classList = 'field__button field__button_flag'
+}
+})
